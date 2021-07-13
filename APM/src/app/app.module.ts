f7f37bd316c7ef8f27bc8aa0d49f27cb84c53c97
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-07-13 08:49:44 -0300

    start with routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,25 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from "@angular/common/http"
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from "./products/product-list.component";
 import { FakeApiComponent } from './fakeapi/fakeapi.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     FakeApiComponent,
     ConverToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full"},
+    ])
   ],
   bootstrap: [AppComponent]
 })
