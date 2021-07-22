8bfa8750013693033317b9a5e593933fd1088d4e
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-07-21 23:51:05 -0300

    ajustes deploy

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "pm-root",
-  templateUrl : "./app.component.html"
+  templateUrl : "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';  
