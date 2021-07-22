bacfe64335cedb07e742f5309a062baab3b1fbcd
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-07-21 22:22:27 -0300

    Product-detail by id

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,13 +11,20 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = "Product Detail"
-  product !: IProduct;
+  product: IProduct | undefined;
+  sub!: Subscription;  
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.sub = this.productService.getProducts().subscribe({
+      next: produtcs => {
+        this.product = produtcs.find((item)=> item.productId == id)
+      }
+    });
   }
 
   onBack(): void{
